6453de44a4a3be0c2e3bf03767412836a25ea30c
Walter Ulreich <walterulreich@Walters-MacBook-Air.local>
2017-08-23 16:47:55 -0600

    commit updated file

--- oop-burgers.js
+++ oop-burgers.js
@@ -19,7 +19,7 @@ var FoodItem = function(name, calories, vegan, glutenFree) {
 
 FoodItem.prototype.stringify = function() {
   // Bacon has 100 calories, is not vegan, and is gluten free.
-  console.log((`${this.name} has ${this.calories} calories, ${this.vegan}, and ${this.glutenFree}.`))
+  return `${this.name} has ${this.calories} calories, ${this.vegan}, and ${this.glutenFree}.`
 }
 
 var lettuce = new FoodItem('lettuce', 5, true, true)
@@ -32,29 +32,40 @@ var bun = new FoodItem('bun', 120, true, false)
 var meat = new FoodItem('meat', 200, false, true)
 var fries = new FoodItem('fries', 365, true, false)
 
-var Plate = function(name, description, price, ingredients) {
+var Plate = function(name, description, price, foodItems) {
   this.name = name
   this.description = description
   this.price = price
-  this.ingredients = ingredients.join(', ')
+  this.foodItems = foodItems
 }
 
 Plate.prototype.stringify = function() {
-  console.log(`The ${this.name} is a ${this.description}. Ingredients: ${this.ingredients}. Price: $${this.price}`)
+  var output = `${this.name}: $${this.price}. ${this.description}. It contains: `
+  for ( var i = 0; i < this.foodItems.length; i++ ) {
+      output += this.foodItems[i].stringify()
+  }
+  return output
 }
 
-var saladPlate = new Plate('salad plate', 'house salad with your choice of dressing', 5, [lettuce.name, dressing.name, tomato.name, onion.name, croutons.name])
-var burgerPlate = new Plate('burger plate', 'burger served with fries', 9, [lettuce.name, tomato.name, onion.name, bun.name, meat.name, fries.name])
+var saladIngredients = [lettuce, dressing, tomato, onion, croutons]
+var burgerIngredients = [lettuce, tomato, onion, bun, meat, fries]
+
+var saladPlate = new Plate('salad plate', 'house salad with your choice of dressing', 5, saladIngredients)
+var burgerPlate = new Plate('burger plate', 'burger served with fries', 9, burgerIngredients)
 
 var Menu = function(name, plates) {
   this.name = name
-  this.plates = plates.join(', ')
+  this.plates = plates
 }
 
-var dinnerMenu = new Menu('dinner menu', [saladPlate.name, burgerPlate.name])
+var dinnerMenu = new Menu('dinner menu', [saladPlate, burgerPlate])
 
 Menu.prototype.stringify = function() {
-  console.log(`The ${this.name} includes the following: ${this.plates}. `)
+  var output = this.name + ': '
+  for ( var i = 0; i < this.plates.length; i++ ) {
+      output += this.plates[i].stringify()
+  }
+  return output
 }
 
 var Restaurant = function(name, description, menu) {
@@ -66,7 +77,7 @@ var Restaurant = function(name, description, menu) {
 var myRestaurant = new Restaurant("Walter's Pub", 'the finest pub around', dinnerMenu)
 
 Restaurant.prototype.stringify = function() {
-  console.log(`${this.name} is commonly referred to as '${this.description}.'`)
+    return `${this.name}: ${this.description}. Check our our menu! ${this.menu.stringify()}`
 }
 
-myRestaurant.stringify()
+console.log(myRestaurant.stringify())
